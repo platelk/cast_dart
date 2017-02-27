f9785f1ca3e762157c1f5fc73bdc155448ebe6fe
platelk <platel.kevin@gmail.com>
2017-02-27 07:55:41 +0100

    first commit

---
+++ lib/cast.dart
@@ -0,0 +1,14 @@
+@JS("cast.framework")
+library cast;
+
+import 'package:js/js.dart';
+import 'package:func/func.dart';
+
+import 'package:cast/chrome.dart';
+export 'package:cast/chrome.dart';
+
+export 'package:cast/src/receiver.dart';
+
+part 'src/cast.dart';
+part 'src/cast_context.dart';
+part 'src/cast_session.dart';
